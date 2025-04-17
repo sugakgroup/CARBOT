bba4eaa1405201a16806dc5d2016b9067eb614ea
sgknsk526 <suga@shuyu.kuchem.kyoto-u.ac.jp>
2025-04-17 13:31:58 +0900

    [upload] simple CARBOT framework uploaded

---
+++ generation.py
@@ -0,0 +1,42 @@
+import glob
+import pandas as pd
+from rdkit import Chem, RDLogger
+import shutil
+import time
+
+from carbot import *
+
+def save_tmt_data(tmt,step,edge_type="unit"):
+    gen = tmt.num_generation
+    pd.to_pickle(tmt,(f"data/tmt_{edge_type}_dist{gen}_step{step}.pkl"))
+    df = [{**{"smi": k},**v} for k,v in tmt.node_data.nodeinfo.items()]
+    df = pd.json_normalize(df)
+    df.to_csv(f'data/tmt_{edge_type}_node_{gen}.csv', index=False, encoding='utf-8')
+    df.to_pickle(f'data/tmt_{edge_type}_node_{gen}.pkl')
+
+    
+from local.evolve import evolve
+
+if __name__ == "__main__":
+    RDLogger.DisableLog('rdApp.*')
+
+    # Set Edge Type
+    # edge_type = "unit"
+    edge_type = "comp"
+
+    # Set Methods (Edge Types) for Transformation if you want
+    evolmethods = tuple([])
+
+    # Check Generation
+    tmts = glob.glob(f'data/tmt_{edge_type}_dist*_step1.pkl')
+    gen = len(tmts)
+
+    # Evolve for one distance
+    if gen == 0:
+        evolmethods = ("connect","vinyl","ethynyl") if edge_type == "unit" else (("connect","vinyl","ethynyl","annulate_pl2","annulate_pl4","phenyl") if edge_type == "comp" else evolmethods)
+        tmt = Transmuter(original_structures=["[H][H]"],evolmethods=evolmethods)
+    else:
+        tmt = pd.read_pickle(f"data/tmt_{edge_type}_dist{gen-1}_step1.pkl")
+        tmt.evolve_single()
+    save_tmt_data(tmt,1,edge_type=edge_type)
+
